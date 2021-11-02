1ca483a3d85a34ce661bfe1074288849cd890ea1
Balaji Chandupatla <balajichandupatla1@gmail.com>
2021-11-02 16:07:02 -0400

    made Changes to the UI

--- app/src/main/java/balaji/ch/balaji_jav1001_scorekeeper/MainActivity.java
+++ app/src/main/java/balaji/ch/balaji_jav1001_scorekeeper/MainActivity.java
@@ -1,14 +1,136 @@
 package balaji.ch.balaji_jav1001_scorekeeper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener, View.OnClickListener {
+
+    private int teamAScore = 0;
+    private int teamBScore = 0;
+    private Boolean teamA = false;
+    private Boolean teamB = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+//        Initializing Score Buttons from 1 to 6
+        Button oneScore = findViewById(R.id.one);
+        Button twoScore = findViewById(R.id.two);
+        Button threeScore = findViewById(R.id.three);
+        Button fourScore = findViewById(R.id.four);
+        Button sixScore = findViewById(R.id.six);
+
+        SwitchCompat switchTeam = findViewById(R.id.teamSide);
+
+        oneScore.setOnClickListener(this);
+        twoScore.setOnClickListener(this);
+        threeScore.setOnClickListener(this);
+        fourScore.setOnClickListener(this);
+        sixScore.setOnClickListener(this);
+
+        switchTeam.setOnCheckedChangeListener(this);
+
     }
+
+    @Override
+    public void onClick(View view){
+        TextView teamATV = findViewById(R.id.teamAScore);
+        TextView teamBTV = findViewById(R.id.teamBScore);
+
+        String teamAScoreString = (String.valueOf(teamATV).split("/")[0]);
+        teamAScore = Integer.parseInt(teamAScoreString);
+
+        String teamBScoreString = (String.valueOf(teamBTV).split("/")[0]);
+        teamBScore = Integer.parseInt(teamBScoreString);
+
+        if (teamA){
+            switch (view.getId()){
+                case R.id.one:
+                    teamAScore += 1;
+                    break;
+                case R.id.two:
+                    teamAScore += 2;
+                    break;
+                case R.id.three:
+                    teamAScore += 3;
+                    break;
+                case R.id.four:
+                    teamAScore += 4;
+                    break;
+                case R.id.six:
+                    teamAScore += 6;
+                    break;
+                default:
+                    teamAScore = 0;
+                    break;
+            }
+            teamATV.setText(teamAScore);
+        }
+        else{
+            switch (view.getId()){
+                case R.id.one:
+                    teamBScore += 1;
+                    break;
+                case R.id.two:
+                    teamBScore += 2;
+                    break;
+                case R.id.three:
+                    teamBScore += 3;
+                    break;
+                case R.id.four:
+                    teamBScore += 4;
+                    break;
+                case R.id.six:
+                    teamBScore += 6;
+                    break;
+                default:
+                    teamBScore = 0;
+                    break;
+            }
+            teamBTV.setText(teamBScore);
+        }
+        Log.d("Team A Score: ", String.valueOf(teamAScore));
+        Log.d("Team B Score: ", String.valueOf(teamBScore));
+
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        if (isChecked){
+//            updateTeamBScore();
+//            teamBTV.setText(teamAScore + "/0");
+            teamB = true;
+        }
+        else{
+//            updateTeamAScore();
+//            teamATV.setText(teamBScore + "/0");
+            teamA = true;
+        }
+    }
+
+//    public void updateTeamAScore(){
+//        TextView teamATV = findViewById(R.id.teamAScore);
+//
+//
+//    }
+//
+//    public void updateTeamBScore(){
+//        TextView teamBTV = findViewById(R.id.teamBScore);
+//    }
 }
